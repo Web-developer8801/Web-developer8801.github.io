404d395b2c7454dee467cbfa9685279a87e085b9
Max <60613745+Web-developer8801@users.noreply.github.com>
2021-01-27 22:05:03 +0700

    commit-2

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 
 
-// Header
+    // Header
 
     $(".header__box-menu ul li").on("click", function (event) {
 
@@ -39,7 +39,7 @@ $(document).ready(function () {
             $current.removeClass('active');
             $current.prev('.project__menu ul li').addClass('active');
 
-           
+
         });
 
         $('.slider-arrows-right').click(function (e) {
@@ -151,6 +151,37 @@ $(document).ready(function () {
         }, 500);
 
     });
+
+    // back-to-top btn
+    (function () {
+        'use strict';
+
+        function trackScroll() {
+            var scrolled = window.pageYOffset;
+            var coords = document.documentElement.clientHeight;
+
+            if (scrolled > coords) {
+                goTopBtn.classList.add('back_to_top-show');
+            }
+            if (scrolled < coords) {
+                goTopBtn.classList.remove('back_to_top-show');
+            }
+        }
+
+        function backToTop() {
+            if (window.pageYOffset > 0) {
+                window.scrollBy(0, -80);
+                setTimeout(backToTop, 0);
+            }
+        }
+
+        var goTopBtn = document.querySelector('.back_to_top');
+
+        window.addEventListener('scroll', trackScroll);
+        goTopBtn.addEventListener('click', backToTop);
+    })();
+
+
     wow = new WOW(
         {
             boxClass: 'wow',      // default
