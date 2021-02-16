da2908d426a4cab6871f2d9b8f850b7ad5533d46
Max <60613745+Web-developer8801@users.noreply.github.com>
2021-02-16 19:46:27 +0700

    commit-8

---
+++ project-2/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+
+{/* <script src="https://kit.fontawesome.com/0e09aa4923.js" crossorigin="anonymous"></script> */}
+  
+  $(function(){
+ // Получить элемент, к которому необходимо добавить маску
+    $(".phone-template").mask("+7(999) 999-9999");
+  });
+});
